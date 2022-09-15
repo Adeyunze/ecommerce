a3095acecb703666c305fe0c34d21e445ca4ab0f
Adeyunze <adeyunze05@gmail.com>
2022-09-15 18:29:37 +0100

    Loading gif

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,7 +4,7 @@ import '../styles/loading.css';
 
 const Loading = () => {
   return (
-    <div className='h-[70vh] flex justify-center items-center'>
+    <div className='h-[50vh] flex justify-center items-center'>
       <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
     </div>
   )
